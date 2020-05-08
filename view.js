4226ef8987dee98d970aeb9ce1f10c4ce0c93a97
marvinbraescher <48102290+marvinbraescher@users.noreply.github.com>
2020-05-07 22:30:17 -0300

    Add files via upload

--- view.js
+++ view.js
@@ -1,15 +1,26 @@
  const express = require('express')
  const app = express()
  const formidable = require('formidable')
- const fs = require('fs')
+ const xlsx = require('xlsx')
  
+
+ //express template engine
+//docblock
 
  app.get('/', function(req, res){
      res.sendFile(__dirname + '/index.html')
  })
+ 
  app.post('/', function(req, res){
+
     var form = new formidable.IncomingForm()
-    form.parse(req)
+    form.parse(req, function(err, fields, files) {
+        let records =  searchJSON(readExcel(files.excel.path), 'NOME', fields.name)
+        res.send('<pre>' + JSON.stringify(records, undefined, 4))
+
+    })
+}) 
+/*
     form.on('fileBegin', function(name, file){
         file.path = __dirname + "/" + file.name
     })
@@ -20,21 +31,57 @@
     })
 
     res.sendFile(__dirname + '/index.html')
-})
-const xlsx = require('xlsx')
 
-var excelToJson = function (name){
+
+    var excelToJson = function (name){
     var excel = xlsx.readFile(__dirname + '/' + name)
     var nameSheet = excel.SheetNames
     var res = (xlsx.utils.sheet_to_json(excel.Sheets[nameSheet[0]]))
     var data = JSON.parse(JSON.stringify(res, function(a, b) {
         return typeof b === "string" ? b.toUpperCase() : b
       }))
-    console.log(data)
+    var result = data.filter(data =>{
+        return new RegExp('LUIZ', 'i').test(data.NOME)
+    })
+    console.log(result)
     }
+
+*/
+
+/**
+ * Trasnforma o arquivo excel em um array de objetos (JSON)
+ * 
+ * @param path caminho do arquivo
+ * @return object[]
+ */    
+var readExcel = function(path){
+    var excel = xlsx.readFile(path)
+    return xlsx.utils.sheet_to_json(excel.Sheets[excel.SheetNames])
+}
+
+/**
+ * Filtra o JSON em busca de objetos com a propriedade e o valor informados
+ * 
+ * @param records lista de registros
+ * @param key propriedade do objeto
+ * @param value valor do campo
+ * @return object[]
+ */    
+var searchJSON = function(records, key, value) {
+    var filterReturn = []
+    for (var i = 0; i < records.length; i++) {
+        if (records[i][key] == undefined) {
+            continue
+        }
+
+        if (records[i][key].toUpperCase().match(value.toUpperCase())) {
+            filterReturn.push(records[i])
+        }
+        
+    }
+    return filterReturn
+}
     
-
-
 
  app.listen(8081, function(){
      console.log('Servidor rodando localhost:8081')
